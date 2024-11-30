30f83c8249ac202950505ebe7cf03b3c9c126f90
Elias Alejandro <elias.alejandro21@gmail.com>
2024-11-30 09:56:17 -0500

    Actualizando controller

--- src/main/java/com/clinica/controller/PacienteController.java
+++ src/main/java/com/clinica/controller/PacienteController.java
@@ -2,6 +2,7 @@ package com.clinica.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,7 @@ public class PacienteController {
     private IPacienteService pacienteService;
 
     @GetMapping("/listar")
+    //@RequestHeader(value = HttpHeaders.AUTHORIZATION) String token
     public List<Paciente> listar() {
         return pacienteService.listar();
     }
