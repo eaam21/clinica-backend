6e0a29e178a82bc9e66d31ee85889d312e21d1a0
Elias Alejandro <elias.alejandro21@gmail.com>
2024-11-30 09:58:04 -0500

    Actualizando security

--- src/main/java/com/clinica/security/SecurityConfig.java
+++ src/main/java/com/clinica/security/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.clinica.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,16 +22,17 @@ public class SecurityConfig {
 	private JwtFilter jwtFilter;
 	
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/login").permitAll()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/user/**").hasRole("USER")
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    	httpSecurity.csrf(csrf->csrf.disable())
+    		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    		.authorizeHttpRequests(http->{
+                http.requestMatchers("/api/usuario/login").permitAll();
+                //http.requestMatchers("/api/paciente/listar").hasRole("ADMIN");
+                http.requestMatchers(HttpMethod.GET, "/api/paciente/listar").hasAnyRole("ADMIN", "DOCTOR");
+                http.anyRequest().authenticated();
+    		})
+    		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    	return httpSecurity.build();
     }
 
     @Bean
