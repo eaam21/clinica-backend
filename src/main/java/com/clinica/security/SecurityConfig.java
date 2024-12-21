01c0cf3035107b9f0fc51d4edcb6dc61a43b790a
coco2023 <coco@gmail.com>
2024-12-20 21:46:02 -0500

    Permisos acceder mantenimiento

--- src/main/java/com/clinica/security/SecurityConfig.java
+++ src/main/java/com/clinica/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.clinica.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +23,9 @@ public class SecurityConfig {
 	
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    	httpSecurity.csrf(csrf->csrf.disable())
+    	httpSecurity
+    		.csrf(csrf->csrf.disable())
+    		.cors(cors->cors.disable())
     		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
     		.authorizeHttpRequests(http->{
                 http.requestMatchers("/api/usuario/login").permitAll();
@@ -35,6 +38,7 @@ public class SecurityConfig {
                 http.anyRequest().authenticated();
     		})
     		.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    	httpSecurity.cors(Customizer.withDefaults());
     	return httpSecurity.build();
     }
 
